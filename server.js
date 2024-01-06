45edf21444eeeac8feb1d8897be1605e195dfc49
Hanna Kotava <hannakotava@Hannas-MacBook-Pro.local>
2024-01-06 16:52:07 +0100

    Update title

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8080;
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('Grupa ZDTESTpol127 jest super SUPER!');
+  res.end('Lorem ipsum test');
 }
 
 const server = http.createServer(requestListener);
